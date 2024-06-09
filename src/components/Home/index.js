f9d78c9f897097340a43c65fb4573199466b2b65
DineshRamaraj <dineshkumarrrr01@gmail.com>
2024-06-09 15:22:06 +0530

    Updating File

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,87 @@
+import Popup from "reactjs-popup";
 import "./index.css";
+import { useState } from "react";
 
-const Home = () => (
-  <div className="home-container">
-    <h1 className="home-title">Create a New Project</h1>
-    <img
-      className="home-image"
-      src="https://res.cloudinary.com/dhwz560kk/image/upload/v1717847794/Group_16_zvlgt6.png"
-      alt="create project"
-    />
-    <p className="home-description">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus
-      dolores corrupti quas accusantium libero cumque nemo id aperiam beatae sit
-      fuga voluptas, vitae velit explicabo repudiandae esse omnis eius amet!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, ad
-      quisquam explicabo ducimus distinctio ab ipsa quaerat facilis corrupti cum
-      quod quam sapiente harum sequi ipsum sed non nam praesentium?
-    </p>
-    <button type="button" className="create-button">
-      <i class="fa-solid fa-plus create-icon"></i>
-      <p className="create-button-text">Create New Project</p>
-    </button>
-  </div>
-);
+const Home = () => {
+  const [error_msg, seterrorMsg] = useState("none");
+  const [inputValue, setInputValue] = useState("");
+
+  const changeInputText = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const errorMsg = (event) => {
+    event.preventDefault();
+    if (inputValue === "") seterrorMsg("block");
+    else seterrorMsg("none");
+  };
+
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Create a New Project</h1>
+      <img
+        className="home-image"
+        src="https://res.cloudinary.com/dhwz560kk/image/upload/v1717847794/Group_16_zvlgt6.png"
+        alt="create project"
+      />
+      <p className="home-description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus
+        dolores corrupti quas accusantium libero cumque nemo id aperiam beatae
+        sit fuga voluptas, vitae velit explicabo repudiandae esse omnis eius
+        amet! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, ad
+        quisquam explicabo ducimus distinctio ab ipsa quaerat facilis corrupti
+        cum quod quam sapiente harum sequi ipsum sed non nam praesentium?
+      </p>
+      <Popup
+        trigger={
+          <button type="button" className="create-button">
+            <i class="fa-solid fa-plus create-icon"></i>
+            <p className="create-button-text">Create New Project</p>
+          </button>
+        }
+        modal
+        nested
+      >
+        {(close) => (
+          <div className="home-popup-container">
+            {/* <button className="close" onClick={close}>
+              &times;
+            </button> */}
+            <h1 className="home-popup-heading">Create Project</h1>
+            <form className="home-popup-form" onSubmit={errorMsg}>
+              <label className="home-popup-label">Enter Project Name</label>
+              <input
+                type="text"
+                className="home-popup-input"
+                placeholder="Type here"
+                onChange={changeInputText}
+              />
+              <p
+                className="home-popup-error-message"
+                style={{ display: error_msg }}
+              >
+                Project Name Can't be empty
+              </p>
+              <div className="home-popup-buttons-container">
+                <button
+                  type="button"
+                  className="home-popup-close-button"
+                  onClick={() => {
+                    close();
+                  }}
+                >
+                  Close
+                </button>
+                <button type="submit" className="home-popup-submit-button">
+                  Create
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
+      </Popup>
+    </div>
+  );
+};
 
 export default Home;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,38 @@
 import "./index.css";
 
+const listItem = [
+  {
+    id: 1,
+    name: "SampleProject",
+    episodes: 4,
+    editingTime: "week",
+  },
+  {
+    id: 2,
+    name: "SampleProject",
+    episodes: 4,
+    editingTime: "week",
+  },
+  {
+    id: 3,
+    name: "SampleProject",
+    episodes: 4,
+    editingTime: "week",
+  },
+  {
+    id: 4,
+    name: "SampleProject",
+    episodes: 4,
+    editingTime: "week",
+  },
+  {
+    id: 5,
+    name: "SampleProject",
+    episodes: 4,
+    editingTime: "week",
+  },
+];
+
 const Projects = () => (
   <div className="project-container">
     <div className="project-header-container">
@@ -11,6 +44,22 @@ const Projects = () => (
         </button>
       </div>
     </div>
+    <ul className="project-list-container">
+      {listItem.map((eachItem) => (
+        <li className="project-item" key={eachItem.id}>
+          <div className="project-logo">{eachItem.name[0]}</div>
+          <div className="project-name-container">
+            <div>
+              <h1 className="project-name">{eachItem.name}</h1>
+              <p className="project-episodes">{eachItem.episodes} Episodes</p>
+            </div>
+            <p className="project-editing-time">
+              Last edited a {eachItem.editingTime} ago
+            </p>
+          </div>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
